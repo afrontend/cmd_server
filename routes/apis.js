16d503f7f96956a1bbaa2ea2af2a76338ac4d95f
afrontend <frontend.wordpress@gmail.com>
2016-12-26 22:00:20 +0900

    그놈 터미널 실행 기능 추가

--- routes/apis.js
+++ routes/apis.js
@@ -3,14 +3,17 @@ var router = express.Router();
 var chalk = require('chalk');
 var error = chalk.bold.blue;
 var info = chalk.bold.blue;
+var cp = require("child_process")
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/terminal', function(req, res, next) {
-  res.send('respond with a resource');
   console.log(info('terminal'));
+  cp.exec("gnome-terminal");
+  res.writeHead(200);
+  res.end();
 });
 
 module.exports = router;
