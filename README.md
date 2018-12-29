f6d9d23dc55fe3efc94bfe3448fe5175bbc33d48
afrontend <frontend.wordpress@gmail.com>
2018-12-29 23:19:02 +0900

    (add) use babel

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
     $ git clone https://github.com/afrontend/cmd_server
     $ cd cmd_server
     $ npm install
+    $ npm build
     $ npm start
 
 ![명령어 서버 스크린샷](https://afrontend.files.wordpress.com/2017/01/cmd_server.png)
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,21 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public/javascripts'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.join(__dirname),
+        exclude: /(node_modules)|(dist)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
   }
 };
 
