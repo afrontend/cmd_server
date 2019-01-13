6bc215f9ffc2b186b9a45803fced303a858a2b72
afrontend <frontend.wordpress@gmail.com>
2019-01-13 23:22:36 +0900

    lint

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var apis = require('./routes/apis');
+const routes = require('./routes/index');
+const users = require('./routes/users');
+const apis = require('./routes/apis');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,7 +29,7 @@ app.use('/apis', apis);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -57,6 +57,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
--- routes/apis.js
+++ routes/apis.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
-var chalk = require('chalk');
-var error = chalk.bold.blue;
-var info = chalk.bold.blue;
-var cp = require("child_process");
-var fs = require('fs');
+const express = require('express');
+const router = express.Router();
+const chalk = require('chalk');
+const error = chalk.bold.blue;
+const info = chalk.bold.blue;
+const cp = require("child_process");
+const fs = require('fs');
 
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -13,20 +13,20 @@ router.get('/', function(req, res, next) {
 function getTerminalCmd(option) {
   option.x = option.x || 0;
   option.y = option.y || 0;
-  var cmd = 'gnome-terminal --geometry=80x24+'+option.x+'+'+option.y;
+  const cmd = 'gnome-terminal --geometry=80x24+'+option.x+'+'+option.y;
   return cmd;
 }
 
 function getVimCmd(filename) {
-  var cmd = 'terminator -e "vim '+filename+'"';
+  const cmd = 'terminator -e "vim '+filename+'"';
   return cmd;
 }
 
 router.get('/terminal', function(req, res, next) {
   console.log(info('query: '+ JSON.stringify(req.query.option, null, 2)));
   if(req.query.option) {
-    var optionString = decodeURIComponent(req.query.option);
-    var optionJson = JSON.parse(optionString);
+    const optionString = decodeURIComponent(req.query.option);
+    const optionJson = JSON.parse(optionString);
     cp.exec(getTerminalCmd(optionJson));
   }
   res.writeHead(200);
@@ -35,7 +35,7 @@ router.get('/terminal', function(req, res, next) {
 
 router.get('/vim', function(req, res, next) {
   console.log(info('query: '+ JSON.stringify(req.query, null, 2)));
-  var filename = req.query.filename;
+  const filename = req.query.filename;
   fs.exists(filename, function () {
     cp.exec(getVimCmd(filename));
   });
